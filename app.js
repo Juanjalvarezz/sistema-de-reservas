c962ce1312439c900c46a8f8816b08a08eaa6e1f
Juan Alvarez <122636634+Juanjalvarezz@users.noreply.github.com>
2023-02-27 19:32:29 -0400

    Actualizando para el uso de las rutas (equipos/espacios)

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//REQUERIMIENTO DE RUTAS
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var equiposRouter = require('./routes/equipos');
+var espaciosRouter = require("./routes/espacios")
+
+
+//APP.USE DE LAS RUTAS
+app.use("/inicio", indexRouter);
+app.use('/equipos', equiposRouter);
+app.use('/espacios', espaciosRouter);
+
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +28,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//APP.USE DE LAS RUTAS
+app.use("/inicio", indexRouter);
+app.use('/equipos', equiposRouter);
+app.use('/espacios', espaciosRouter);
+app.use("/serial", equiposRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,6 +44,19 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);  
+  res.render('error');
+});
+
+//Creando el mensaje del listener del servidor
+var PORT = 3001
+app.listen(PORT, function(err){
+  if (err) console.log(err);
+  console.log("Server listening on PORT", PORT);
+}); 
+module.exports = app;
 
   // render the error page
   res.status(err.status || 500);  
