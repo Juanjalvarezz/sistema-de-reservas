aa72314f5d248c1c9dc4cb41ad22d1f5564b619e
Juan Alvarez <122636634+Juanjalvarezz@users.noreply.github.com>
2023-02-28 12:37:17 -0400

    Correccion de errores

--- app.js
+++ app.js
@@ -12,12 +12,6 @@ var trabajosRouter = require('./routes/trabajos');
 
 var app = express();
 
-//APP.USE DE LAS RUTAS
-app.use("/inicio", indexRouter);
-app.use('/equipos', equiposRouter);
-app.use('/espacios', espaciosRouter);
-app.use('/trabajos', trabajosRouter);
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -33,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use("/inicio", indexRouter);
 app.use('/equipos', equiposRouter);
 app.use('/espacios', espaciosRouter);
-app.use("/serial", equiposRouter);
+app.use('/trabajos', trabajosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
